0cc4d6e58f89ab95c1b73377fb959e4c30c0d0ba
ElectroWang⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-08-24 12:13:56 -0400

    Create Gui.java

---
+++ Gui.java
@@ -0,0 +1,11 @@
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+
+public class Gui{
+    public static void main(String[] args){
+        JFrame jframe=new JFrame("kkkkkkk");
+        jframe.setLayout(new FlowLayout());
+        jframe.setSize(100,100);
+    }
+}
