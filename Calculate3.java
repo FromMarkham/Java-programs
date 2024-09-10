9b23b2c9ec62981a8fe86b8b8f631b51c0329075
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-09-10 12:05:38 -0400

    Create Calculate3.java

---
+++ Calculate3.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner; //import from Java utility package 
+
+
+public class Calculate3{
+	public static void main(String[]args) {
+		int integer1;
+		int integer2;
+		int integer3;
+		
+		System.out.println("Enter 3 integers");
+		Scanner input1=new Scanner(System.in);
+		
+		//System.out.println("Enter the second integer");
+		Scanner input2=new Scanner(System.in);
+		
+		//System.out.println("Enter the third integer");
+		Scanner input3=new Scanner(System.in);
+		
+		
+		integer1=input1.nextInt();
+		integer2=input2.nextInt();
+		integer3=input3.nextInt();
+		
+		int product=integer1*integer2*integer3;
+		int sum=(integer1*integer2*integer3);
+		int average=(integer1*integer2*integer3)/3;
+		
+		int[] integers=new int[] {integer1,integer2,integer3};
+		
+		int smallest=Arrays.stream(integers).min().getAsInt();
+		int largest=Arrays.stream(integers).max().getAsInt();
+		
+		System.out.printf("This is the product of your 3 integers: %d",product);
+		System.out.printf("This is the product of your 3 integers: %d",product);
+		System.out.printf("This is the product of your 3 integers: %d",product);
+		System.out.printf("This is the product of your 3 integers: %d",product);
+		System.out.printf("This is the product of your 3 integers: %d",product);
+		
+	}
+}
