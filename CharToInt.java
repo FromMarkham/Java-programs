33781001ac1393063bba0b50739a00708c921e4e
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-09-27 16:19:42 -0400

    Create CharToInt.java

---
+++ CharToInt.java
@@ -0,0 +1,42 @@
+//name: Bob Wang- CharToInt.java
+
+//Date: September 12 2024 
+//Course: ICS3U1 (gr 11 computer science)
+//Description:Write an application that displays the integer equivalents of some uppercase letters, 
+//lowercase letters, digits and special symbols.  You will need to cast the character. 
+
+
+
+//Skills: i can import libraries, use math operations,  get the program to accept inputs and use java classes, variables, and print statements
+//And find the integer values of various characters 
+
+import java.util.Scanner; //import from Java utility package so the program can accept inputs 
+
+
+//the main class
+class CharToInt{
+	//the main method 
+	public static void main(String[]args) {
+
+
+		
+
+		//values of the various characters I chose 
+		System.out.println("The value of the character A is:"+(int) 'A');
+		System.out.println("The value of the character a is:"+(int) 'a');
+		System.out.println("The value of the character B is:"+(int) 'B');
+		System.out.println("The value of the character c is:"+(int) 'c');
+		System.out.println("The value of the character D is:"+(int) 'D');
+		System.out.println("The value of the character . is:"+(int) '.');
+		System.out.println("The value of the character / is:"+(int) '/');
+
+		System.out.println("The value of the character 5 is:"+(int) '5');
+		System.out.println("The value of the character 2 is:"+(int) '2');
+		System.out.println("The value of the character 3 is:"+(int) '3');
+		System.out.println("The value of the character I is:"+(int) 'I');	
+		
+
+
+		
+	}
+}
