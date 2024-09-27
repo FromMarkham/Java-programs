5a442de674c9a60e555d6c799d87753843e2cc54
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-09-27 16:17:56 -0400

    Create frame.java

---
+++ frame.java
@@ -0,0 +1,15 @@
+//Name: Bob wang, frame.java
+//Description: Create a BMI Calculator application that reads the user’s weight and height (either Metric or Imperial system) and then calculates and displays the user’s BMI.
+//date: September 22,2024
+//Skills: imports, GUI, variables, classes, objects, Java syntax
+
+public class BMICalculator{ //Main class
+    public static void main(String[] args){ //Main method 
+
+        //putting the brackets calls a method called the constructor which creates the instance of a new class
+    	//Calls the frame which contains all the code for the BMI calculator 
+    	new frame();
+        
+      
+    }
+}
