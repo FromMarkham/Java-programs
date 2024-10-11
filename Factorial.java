d1de1ce185c87a9e50432413c46860fd4f25f87b
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-10-11 16:51:41 -0400

    Add files via upload

--- Factorial.java
+++ Factorial.java
@@ -1,28 +1,41 @@
-import java.util.Scanner;
+import java.util.Scanner; //import 
 
-public class Factorial{
-	public static void main(String[]args) {
+//Date: October 11 2024 
+//Name: Bob Wang, Factorial.java
+//Description: Write an application that reads a nonnegative integer from an input dialog and computes and prints its factorial.
+//Skills: while loops, incrementation, algorithms, math, java syntax, classes, inputs, printing to the screen
+
+public class Factorial{ //main class
+	
+	public static void main(String[]args) { //main method
 		
-		int number;
-		int factorial;
-		int counter;
+		int number; //Variable to hold the users input
+		
+		int factorial; //Variable to hold the factorial
+		int counter; //Counter for the while loop
+		
 		counter=1;
 		factorial=1;
 		
 		Scanner input=new Scanner(System.in);
-		System.out.println("Enter a factorial");
-		number=input.nextInt();
+		System.out.println("Enter a factorial"); 
+		
+		number=input.nextInt(); //Ask the user for the input 
+
 		
 		
-		while (counter<=number) {
+		while (counter<=number) { //In a while loop, whilst the counter is smaller then the user’s input….
+
 			
-			factorial=factorial*counter;
+			factorial=factorial*counter; //Multiply this variable that is initially set to 1 by the counter 
+
 			
-			counter++;
+			counter++; //Increment the counter
 			
 		}
 		
-		System.out.printf("The factorial of %d is equal to %d",number,factorial);
+		System.out.printf("The factorial of %d is equal to %d",number,factorial); //Print the factorial to the screen. 
+
 		
 		
 	}
