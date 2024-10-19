54b4c2048cb0a8009887370e892f1d3dafb62343
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-10-19 17:19:49 -0400

    Add files via upload

---
+++ Triangles.java
@@ -0,0 +1,89 @@
+
+public class Triangles{
+	
+	public static void main(String[]args) {
+		
+		for (int k=0; k<=10; k++) {
+			
+			System.out.println();
+			
+			
+			for (int y=0; y<=k; y++) {
+				
+				System.out.printf("*");
+				
+				
+			}
+			
+			
+		}
+		
+		
+		for (int a=0; a<=10; a++) {
+			
+			System.out.println();
+			
+			int b=10;
+			b=b-a;
+			
+			for (int c=0; c<=b; c++) {
+				
+				System.out.printf("*");
+				
+				
+			}
+			
+		}
+		//triangles c&d
+		
+		for (int k=0; k<=10; k++) {
+				
+			System.out.println();
+			
+			int z=10-k;
+				
+			for (int y=0; y<=z; y++) {
+				
+				System.out.printf(" ");
+					
+					
+			}
+			
+			for (int y=0; y<=k; y++) {
+					
+				System.out.printf("*");
+					
+					
+			}
+				
+				
+		}
+						
+		for (int a=0; a<=10; a++) {
+			
+			System.out.println();
+			
+			int b=10;
+			b=b-a;
+			
+			for (int r=0; r<=a; r++) {
+				
+				System.out.printf(" ");
+				
+				
+			}
+			
+			for (int c=0; c<=b; c++) {
+				
+				System.out.printf("*");
+				
+				
+			}
+			
+
+			
+		}		
+
+				
+		}			
+	}		
