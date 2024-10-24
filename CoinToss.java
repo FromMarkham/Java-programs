9f0f577ee636d904de91a10fd1d22a665fbe0906
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-10-24 08:27:02 -0400

    Add files via upload

--- CoinToss.java
+++ CoinToss.java
@@ -1,116 +1,113 @@
+//Date: october 23 2024 
+//Description: program that simulates a coin toss
+//Skills: modules, methods, imports, inputs,  java syntax,  math.random
+//Name: bob Wang, CoinToss.java
 
 import java.util.Scanner;
-import java.lang.Math;
 
-public class CoinToss{
+public class CoinToss {
 	static int headscount;
 	static int tailscount;
-	
-	public static void main(String[]args) {
-		
+
+	public static void main(String[] args) {
+
 		boolean tossornot;
 		boolean trueorfalse;
 
 		int headscount2;
-		
+
 		int tailscount2;
-		
-		tossornot=true;
-		
+
+		tossornot = true;
+
 		do {
-		
-		
-		tossornot=coinflipornot();
-		
-		trueorfalse=flipthecoin();
-		
-		headscount2=countheads(trueorfalse);
-		tailscount2=counttails(trueorfalse);
-		
-		headsortails(headscount2,tailscount2);
-		
-		}while(tossornot);
-		
-		
-		
+
+			tossornot = coinflipornot();
+			
+
+			trueorfalse = flipthecoin();
+
+			// pass in whether theree was a heads or not into the head counter
+			headscount2 = countheads(trueorfalse);
+
+			// pass in whether there was a tails or not into the tails counter
+			tailscount2 = counttails(trueorfalse);
+
+			// keep count of the heads and tails
+			headsortails(headscount2, tailscount2);
+
+			// keep doing all of the above whilst the user says yes
+		} while (tossornot);
+
 	}
-	
+
 	private static boolean coinflipornot() {
-		
+
 		String yesorno;
-		Scanner input=new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		
-		yesorno=input.next();
+		System.out.println(" ");
+		System.out.printf("Do you wanna flip the coin? If yes, type Toss");
+		yesorno = input.next();
 		
+
 		if (yesorno.equalsIgnoreCase("Toss")) {
 			return true;
-		}
-		else {
-			
+		} else {
+
 			return false;
 		}
 	}
-		
+
 	private static boolean flipthecoin() {
-		
+
 		double randomno;
-		
+
 		int randomno2;
-		
-		randomno=Math.random();
-		
-		randomno2=(int) Math.ceil(randomno);
-		
-		if (randomno2==0) {
-			
+
+		randomno = Math.random();
+
+		randomno2 = (int) Math.round(randomno);
+
+		if (randomno2 == 0) {
+
 			return true;
 		}
-		
+
 		else {
-			
+
 			return false;
 		}
-		
+
 	}
-	
+
 	private static int counttails(boolean trueorfalse) {
-		
-		
-		if (trueorfalse==false) {
-			
+
+		if (trueorfalse == false) {
+
 			tailscount++;
 		}
-		
+
 		return tailscount;
-		
+
 	}
 
 	private static int countheads(boolean trueorfalse) {
-		
-		
-		
-		if (trueorfalse==true) {
-			
-			
-			
+
+		if (trueorfalse == true) {
+
 			headscount++;
-			
+
 		}
-		
+
 		return headscount;
-	
-		
+
 	}
-	
-	private static void headsortails(int headscount2,int tailscount2) {
-		
-			
-		
-			System.out.printf("%d heads, %d tails",headscount2,tailscount2);
-		
-			
-		
+
+	private static void headsortails(int headscount2, int tailscount2) {
+
+		System.out.printf("%d heads, %d tails", headscount2, tailscount2);
+
 	}
-	
-	
+
 }
