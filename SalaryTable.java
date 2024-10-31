54def818aea9dcc8bacfe68bbb45caa2a2078502
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-10-31 09:07:37 -0400

    Add files via upload

--- SalaryTable.java
+++ SalaryTable.java
@@ -1,6 +1,11 @@
 
 import java.util.Scanner;
 
+//Date: October 31 2024 
+
+//Description: Write an application that determines how many of the salespeople earned salaries in each of the following ranges 
+//Skills: arrays, inputs, tables, printf, methods 
+
 public class SalaryTable{
 	
 	
@@ -17,18 +22,25 @@ public class SalaryTable{
 		
 		int salary;
 		
+		//Ask the user for a salary 
 		salary=askuserforsalaries();
+		
+		//Classify the salary into a range using an array 
 		thesalaries=classifysalaries(salary);
 		
+		//Ask the user if there are more employees 
 		System.out.println("Are there more employees. If u wanna continue type Y, if not, type anything else");
 		yesorno=input.next();
 		
+		//Repeat whilst the user says Y in the console 
 		}while (yesorno.equalsIgnoreCase("Y"));
 		
+		//If the user types something other then y in the console display all the salaries the user entered 
 		displaytheresults(thesalaries);
 		
 	}
 	
+	//Method to ask the user for salaries 
 	private static int askuserforsalaries() {
 		
 		System.out.println("Enter da salary of anemployees ");
@@ -36,6 +48,7 @@ public class SalaryTable{
 		
 	}
 	
+	//Method to classify all the salaries the user entered 
 	private static int[] classifysalaries(int thesalary) { //so mething is wrong here
 		
 		if (thesalary/100>=10) {
@@ -51,6 +64,7 @@ public class SalaryTable{
 		
 	}
 	
+	//Method to display how many people earned salaries in the various ranges 
 	private static void displaytheresults(int []salaries) {
 		
 		
@@ -73,4 +87,3 @@ public class SalaryTable{
 	
 	
 }
-
